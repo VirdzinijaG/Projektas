ead2ee92c8c25331fd7f11ba11abeed72d90af31
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-07-27 21:00:40 +0300

    json, propjektas

---
+++ src/Puodukas.js
@@ -0,0 +1,9 @@
+class Puodukas {
+    constructor() {
+        this.spalva = "baltas";
+        this.turis = 150;
+        this.kiekis = 0;
+    }
+}
+
+export { Puodukas }
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Puodukas } from "./Puodukas.js";
+
+console.log("Hello world");
+
+let p = new Puodukas();
+
+console.log(p);
